85a1c5aa584077f3f529110565c7c6cf240426b5
Marsiio <eros6759@gmail.com>
2022-06-29 22:34:57 +0200

    Splash Screen

---
+++ app/src/main/java/it/pdm/app/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package it.pdm.app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(this.mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 6000)
+    }
+}
