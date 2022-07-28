7ae71a9f122040c6b12d7f74dd62485553048f59
Marsiio <eros6759@gmail.com>
2022-07-28 17:45:39 +0200

    aggiunti fragment e navigationUI

--- app/src/main/java/it/pdm/app/MainActivity.kt
+++ app/src/main/java/it/pdm/app/MainActivity.kt
@@ -2,11 +2,44 @@ package it.pdm.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import it.pdm.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNav.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.homeFragment -> replaceFragment(Home())
+                R.id.petFragment -> replaceFragment(Pet())
+                R.id.shopFragment -> replaceFragment(Shop())
+
+                else ->{
+
+
+                }
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_fragment, fragment)
+        fragmentTransaction.commit()
     }
 }
