e0615938b7056a217339b52a360f46891e002dc2
Marsiio <eros6759@gmail.com>
2022-07-29 15:38:26 +0200

    update navbar

--- app/src/main/java/it/pdm/app/MainActivity.kt
+++ app/src/main/java/it/pdm/app/MainActivity.kt
@@ -11,35 +11,18 @@ import it.pdm.app.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(Home())
 
-        binding.bottomNav.setOnItemSelectedListener {
+        navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
+        setupWithNavController(binding.bottomNavigationView, navController)
 
-            when(it.itemId){
-                R.id.homeFragment -> replaceFragment(Home())
-                R.id.petFragment -> replaceFragment(Pet())
-                R.id.shopFragment -> replaceFragment(Shop())
-
-                else ->{
-
-
-                }
-            }
-            true
-        }
 
     }
 
-    private fun replaceFragment(fragment : Fragment){
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.main_fragment, fragment)
-        fragmentTransaction.commit()
-    }
 }
