36b1efe51ac995c96de9dd3283a8311a889f1745
Marsiio <eros6759@gmail.com>
2022-08-04 01:16:27 +0200

    add toolbar

--- app/src/main/java/it/pdm/app/BlankFragment.kt
+++ app/src/main/java/it/pdm/app/BlankFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import it.pdm.app.databinding.FragmentBlankBinding
+import it.pdm.app.databinding.FragmentHomeBinding
 
 class BlankFragment : Fragment() {
+
+    private lateinit var binding: FragmentBlankBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        binding = FragmentBlankBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
+
+   fun onBackPressed(){
+
+   }
 
 }
--- app/src/main/java/it/pdm/app/MainActivity.kt
+++ app/src/main/java/it/pdm/app/MainActivity.kt
@@ -3,16 +3,21 @@ package it.pdm.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import it.pdm.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
         setupWithNavController(binding.bottomNavigationView, navController)
+
 
 
         //quando non siamo nei fragment principali la navbar viene nascosta
@@ -34,7 +40,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        val navController = findNavController(R.id.activity_main_nav_host_fragment)
+        val config = AppBarConfiguration(navController.graph)
+        //findViewById<Toolbar>(R.id.toolbarTb).setupWithNavController(navController, config)
 
+        toolbar = binding.toolbarTb
+        setSupportActionBar(toolbar)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id != R.id.petFragment && destination.id != R.id.homeFragment && destination.id != R.id.settingsFragment) {
+                    supportActionBar?.show() // to show
+                    findViewById<Toolbar>(R.id.toolbarTb).setupWithNavController(navController, config)//per tornare indietro nei fragments
+                }else{
+                supportActionBar?.hide() // to hide
+            }
+        }
     }
-
 }
+
