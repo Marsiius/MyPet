4918c34d39395f7f1b75d2a69a89f00265579ca3
Marsiio <eros6759@gmail.com>
2022-10-24 19:42:56 +0200

    Create stepCounter w/o UI

--- app/src/main/java/it/pdm/app/Settings.kt
+++ app/src/main/java/it/pdm/app/Settings.kt
@@ -1,12 +1,24 @@
 package it.pdm.app
 
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+//import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 
-class Settings : Fragment() {
+class Settings : Fragment(), SensorEventListener {
+
+    var running = false
+    var sensorManager: SensorManager? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +28,38 @@ class Settings : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepsSensor == null) {
+            Toast.makeText(activity, "No Step Counter Sensor !", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+    }
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (running) {
+            stepsValue.setText("" + event.values[0])
+        }
+    }
+
 }
+
