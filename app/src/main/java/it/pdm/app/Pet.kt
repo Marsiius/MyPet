7c5a166d73265399050125f5d2a72170c4828e3e
canali.luca <canali.luca998@gmail.com>
2022-10-26 18:29:49 +0200

    Update Pet.kt
    Clean code and update Pet class

--- app/src/main/java/it/pdm/app/Pet.kt
+++ app/src/main/java/it/pdm/app/Pet.kt
@@ -1,19 +1,17 @@
 package it.pdm.app
 
 import android.content.res.ColorStateList
-import android.graphics.Color
 import android.graphics.Color.*
 import android.os.Bundle
-import android.transition.TransitionInflater
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_pet.*
 
 var clicked = false
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -25,9 +23,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Pet : Fragment() {
-
+    //override per la definizione della parte grafica/layout del fragment
+    //questo metodo deve ritornare una vista oppure null (default)
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
@@ -35,40 +35,35 @@ class Pet : Fragment() {
     }
 
     //implementazione FAB botton con override di onViewCreated
+    //Viene chiamato dopo onCreateView() e viene utilizzato principalmente per le inizializzazioni-
+    //-finali (ad esempio, la modifica degli elementi dell'interfaccia utente);-
+    //-questo è deprecato dal livello API 28.
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
-        val fabAdd = view.findViewById<FloatingActionButton>(R.id.fab_add)
-        val fabEdit = view.findViewById<FloatingActionButton>(R.id.fab_edit)
-        val fabDel = view.findViewById<FloatingActionButton>(R.id.fab_del)
-        val open = AnimationUtils.loadAnimation(context, R.anim.fab_open)
-        val forward = AnimationUtils.loadAnimation(context, R.anim.rotate_forward)
-        val backward = AnimationUtils.loadAnimation(context,R.anim.rotate_backward)
-        val colorOpen = Color.rgb(255,0,0)
-        val colorClose = Color.rgb(76, 175, 80)
-
         fab.setOnClickListener{
-            if(!clicked){
-                //fab.startAnimation(open)
-                fab.startAnimation(forward)
-                //fab.setRippleColor(WHITE) //Cambia il colore SOLO QUANDO viene premuto
-                fab.backgroundTintList = ColorStateList.valueOf(colorOpen)
-                fabAdd.visibility = View.VISIBLE
-                fabEdit.visibility = View.VISIBLE
-                fabDel.visibility = View.VISIBLE
-            }else{
-                //fab.startAnimation(close)
-                fab.startAnimation(backward)
-                fab.backgroundTintList = ColorStateList.valueOf(colorClose)
-                fabAdd.visibility = View.INVISIBLE
-                fabEdit.visibility = View.INVISIBLE
-                fabDel.visibility = View.INVISIBLE
-            }
-            clicked = !clicked
+            setFabAnimation()
         }
     }
 
+    private fun setFabAnimation(){
+        if(!clicked){
+            fab.startAnimation(AnimationUtils.loadAnimation(context,R.anim.rotate_forward))
+            fab.backgroundTintList = ColorStateList.valueOf(rgb(255,0,0))
+            fab_add.visibility = View.VISIBLE
+            fab_edit.visibility = View.VISIBLE
+            fab_del.visibility = View.VISIBLE
+        }else{
+            fab.startAnimation(AnimationUtils.loadAnimation(context, R.anim.rotate_backward))
+            fab.backgroundTintList = ColorStateList.valueOf(rgb(76,175,80))
+            fab_add.visibility = View.INVISIBLE
+            fab_edit.visibility = View.INVISIBLE
+            fab_del.visibility = View.INVISIBLE
+        }
+        clicked = !clicked
+    }
+
+    //companion object da implementare per il salvataggio di contesto
     companion object {
         /**
          * Use this factory method to create a new instance of
