010f50a2f4725c059975e1a0756c45fceb2e2059
canali.luca <canali.luca998@gmail.com>
2022-11-09 16:01:41 +0100

    Complete Login and Sign Up

--- app/src/main/java/it/pdm/app/LoginActivity.kt
+++ app/src/main/java/it/pdm/app/LoginActivity.kt
@@ -1,11 +1,70 @@
 package it.pdm.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailTV: TextView
+    private lateinit var passwordTV: TextView
+    private lateinit var logbutton: Button
+    private lateinit var signinTV: TextView
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        initializeUI()
+
+        logbutton.setOnClickListener{
+            loginUserAccount()
+        }
+        signinTV.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun loginUserAccount() {
+        val email = emailTV.text.toString()
+        val password = passwordTV.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if (it.isSuccessful) {
+                    Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(applicationContext, "Login failed! Please try again later", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun initializeUI() {
+        emailTV = findViewById(R.id.et_email)
+        passwordTV = findViewById(R.id.et_password)
+        signinTV = findViewById(R.id.textView3)
+        logbutton = findViewById(R.id.button_login)
     }
 }
--- app/src/main/java/it/pdm/app/RegisterActivity.kt
+++ app/src/main/java/it/pdm/app/RegisterActivity.kt
@@ -1,11 +1,82 @@
 package it.pdm.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var emailTV: TextView
+    private lateinit var passwordTV: TextView
+    private lateinit var confirmPasswordTV: TextView
+    private lateinit var regButton: Button
+    private lateinit var loginTV: TextView
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mAuth = FirebaseAuth.getInstance()
+        initializeUI()
+
+        regButton.setOnClickListener{
+            registerNewUser()
+        }
+
+        loginTV.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun registerNewUser() {
+
+        val email = emailTV.text.toString()
+        val password = passwordTV.text.toString()
+        val confirmation = confirmPasswordTV.text.toString()
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(applicationContext, "Please, enter email", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password) || password.length < 6) {
+            Toast.makeText(applicationContext, "Invalid password", Toast.LENGTH_LONG).show()
+            return
+        }
+        if(TextUtils.isEmpty(confirmPasswordTV.toString())){
+            Toast.makeText(applicationContext, "Please confirm password!", Toast.LENGTH_LONG).show()
+            return
+        }
+        if(password != confirmation){
+            Toast.makeText(applicationContext, "Confirm password error: try again", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if(it.isSuccessful){
+                    Toast.makeText(applicationContext, "Registration successful!", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, LoginActivity::class.java )
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(applicationContext, "Registration failed! You have already account", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun initializeUI() {
+        emailTV = findViewById(R.id.et_email)
+        passwordTV = findViewById(R.id.et_password)
+        confirmPasswordTV = findViewById(R.id.et_confirm_password)
+        regButton = findViewById(R.id.button_signin)
+        loginTV = findViewById(R.id.textView3)
     }
 }
