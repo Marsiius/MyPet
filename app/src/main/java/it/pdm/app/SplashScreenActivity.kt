1a09928919d528b49b78d9d11a996844afc83aef
luca.cana <88391203+Cannuzza@users.noreply.github.com>
2022-11-10 10:38:49 +0100

    Update AndroidManifest.xml

--- app/src/main/java/it/pdm/app/SplashScreenActivity.kt
+++ app/src/main/java/it/pdm/app/SplashScreenActivity.kt
@@ -11,7 +11,7 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         Handler(this.mainLooper).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, RegisterActivity::class.java))
             finish()
         }, 6000)
 
