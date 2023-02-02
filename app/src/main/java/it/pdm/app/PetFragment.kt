affae2aae67b27810ef7b3d101d51da20a2246b3
luca.canali <canali.luca998@gmail.com>
2023-02-02 10:21:00 +0100

    Fixed image picture

--- app/src/main/java/it/pdm/app/PetFragment.kt
+++ app/src/main/java/it/pdm/app/PetFragment.kt
@@ -197,17 +197,19 @@ class Pet : Fragment() {
     //metodo che scarica la foto dal DB Storage oppure, se presente sul dispositivo, viene caricata da esso
     private fun setPicturePet(){
         val directory = context?.getDir("imageDir", Context.MODE_PRIVATE)
-        val file = File(directory, "your-image.jpg")
+        val file = File(directory, "pet_picture.jpg")
         if(file.exists()){
             val bitmap = BitmapFactory.decodeFile(file.absolutePath)
             if(pet_picture!=null)
                 pet_picture.setImageBitmap(bitmap)
+
+
         }else{
             val refPicture = FirebaseDBHelper.dbRefST.child("images/pet_picture.jpg")
             refPicture.getBytes(1000000000000000000).addOnSuccessListener {
                 val bitmap = BitmapFactory.decodeByteArray(it,0,it.size)
                 if(bitmap!=null){
-                    //saveBitmapToInternalStorage(bitmap)
+                    saveBitmapToInternalStorage(bitmap)
                     if(pet_picture!=null)
                         pet_picture.setImageBitmap(bitmap)
                 }
@@ -232,12 +234,16 @@ class Pet : Fragment() {
 
     //metodo che prende in input un bitmap e lo salva in una cartella della app
     private fun saveBitmapToInternalStorage(bitmap: Bitmap?) {
-        val directory = context?.getDir("imageDir", Context.MODE_PRIVATE)
-        val file = File(directory, "your-image.jpg")
-        val stream = FileOutputStream(file)
-        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-        stream.flush()
-        stream.close()
+        val directory =  context?.getDir("imageDir", Context.MODE_PRIVATE)
+        val file = File(directory, "pet_picture.jpg")
+        try {
+            val stream = FileOutputStream(file)
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        }
     }
 
     //metodo che imposta la visibilità degli elementi del fragment che fanno riferimento all'animale
